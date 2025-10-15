3b896a2c905d4eaa3b9dd141bfd5fd5378ab443e
Anri Maidla <amaidla@hkhk.edu.ee>
2025-10-15 15:13:35 +0300

    Add files via upload

---
+++ nr1.py
@@ -0,0 +1,25 @@
+# Harjutus 1
+# Anri Maidla 15.10.2025
+
+# print (Tere\"suur\" Maailm')
+# Kolmnurk
+import turtle
+#turtle.up()
+#turtle.goto(-400,300)
+#turtle.down()
+#for i in range(4):
+    #turtle.forward(200)
+    #turtle.right(120)
+# Oma nime tähed    
+
+turtle.forward(150)
+turtle.right(150)
+turtle.forward(150)
+turtle.backward(70)
+turtle.right(90)
+turtle.forward(50)
+
+
+    
+    
+turtle.done()
