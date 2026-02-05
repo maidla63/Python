a94aecd1025c72509651d769280913db404ff4e5
Anri Maidla <amaidla@hkhk.edu.ee>
2026-02-05 15:52:36 +0200

    1
    1

---
+++ ylikooli yl/nr04.3.py
@@ -0,0 +1,10 @@
+def eelarve(arv):
+    summa = arv * 55
+    return summa
+kutsutud = int(input("Mitu inimest on kutsutud? "))
+tuleb = int(input("Mitu inimest tuleb? "))
+
+print(eelarve(kutsutud))
+print(eelarve(tuleb))
+
+
