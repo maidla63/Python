9f4d884ee9b819e4d8660512aa999603125cdf0f
Anri Maidla <amaidla@hkhk.edu.ee>
2026-02-11 09:08:23 +0200

    Update nr13.py

--- nr13.py
+++ nr13.py
@@ -9,19 +9,40 @@ pikkus = screen.numinput("Pikkuse sisetamine", "Kui pikka on vaja", default=20, 
 pikk = 10
 
 
-number = 1 
+number = 0
 for i in range(1, int(pikkus)): 
     t.lt(90)
-    t.fd(20+pikk)
-    t.write(number, font=("Arial", 12, "normal"))
-    t.bk(20+pikk)
+    t.fd(10+pikk)
+    t.bk(10+pikk)
     t.rt(90)
     t.fd(20)
     if i%5==0:
         pikk = 10
     else:
         pikk = 0 
-        number= " "
-    number += i
+       # number= "  "
+    # number += i
+    
+t.goto(0,0)
+t.lt(90)
+t.fd(10+pikk+20)
+t.rt(90)
+t.penup()
+for i in range(1, int(pikkus)): 
+        if number%5==0:
+            t.write(number, font=("Arial", 8, "normal"))
+        t.fd(20)
+        number+=1
 
+
+
+
+
+
+
+
+
+
+
+t.hideturtle
 t.done()
