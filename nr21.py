4c44f79e08f21010f5d9a6201a1d106943198f0b
Anri Maidla <amaidla@hkhk.edu.ee>
2026-03-06 09:59:08 +0200

    Update nr21.py

--- nr21.py
+++ nr21.py
@@ -4,3 +4,8 @@
 # muudab halltoonideks (“L”)
 # kärbib 350×350 pisipildiks
 # salvestab kataloogi /thumb ja nummerdab nimed 1.jpg, 2.jpg jne
+
+from PIL import Image
+
+img = Image.open("yl_pildid")
+ 
