4d9cad837fb12170dd65a6576b375411a22d10ba
Anri Maidla <amaidla@hkhk.edu.ee>
2026-03-06 09:53:51 +0200

    1
    1

---
+++ nr21.py
@@ -0,0 +1,6 @@
+# Loo kood, mis:
+# leiab kataloogist kõik JPG pildid
+# pöörab pildid otseks
+# muudab halltoonideks (“L”)
+# kärbib 350×350 pisipildiks
+# salvestab kataloogi /thumb ja nummerdab nimed 1.jpg, 2.jpg jne
